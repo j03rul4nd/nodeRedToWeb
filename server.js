a16aaf7864fda580489fe1ea55d04ab2f4a7fac3
joel <joelbenitezdonari@gmail.com>
2024-06-21 20:11:15 +0200

    first commit node red users

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const http = require('http');
 const express = require('express');
 const RED = require('node-red');
@@ -5,12 +7,22 @@ const RED = require('node-red');
 const app = express();
 const server = http.createServer(app);
 
+const users = [
+  { username: "admin", password: bcrypt.hashSync(process.env.ADMIN_PASSWORD || 'admin', 8), permissions: "*" },
+  { username: "user1", password: bcrypt.hashSync(process.env.USER1_PASSWORD || 'user1password', 8), permissions: "read" },
+  { username: "user2", password: bcrypt.hashSync(process.env.USER2_PASSWORD || 'user2password', 8), permissions: "read" }
+];
+
 const settings = {
   httpAdminRoot: "/",
   httpNodeRoot: "/api",
   userDir: "./",
   flowFile: 'flows.json',
-  credentialSecret: process.env.CREDENTIAL_SECRET || 'your-secret-key', 
+  credentialSecret: process.env.CREDENTIAL_SECRET || 'rerer',
+  adminAuth: {
+    type: "credentials",
+    users: users
+  },
   functionGlobalContext: {}
 };
 
@@ -19,7 +31,6 @@ RED.init(server, settings);
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 
-// Usar el puerto definido por Render en la variable de entorno PORT, o 8000 por defecto
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, function() {
